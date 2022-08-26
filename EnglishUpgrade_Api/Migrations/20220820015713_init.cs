588fe0178d29e24e471dceeb6eee3d345f0c7d28
misaelortizdp <misaelortiz58@gmail.com>
2022-08-26 12:33:51 -0500

    data migration added

---
+++ EnglishUpgrade_Api/Migrations/20220820015713_init.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnglishUpgradeApi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    iduser = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    email = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    grouprank = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    companyrank = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    level = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.iduser);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scores",
+                columns: table => new
+                {
+                    idscores = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    yearParticipate = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    comprehension = table.Column<int>(type: "int", nullable: true),
+                    fluency = table.Column<int>(type: "int", nullable: true),
+                    sentenceStructure = table.Column<int>(type: "int", nullable: true),
+                    pronunciation = table.Column<int>(type: "int", nullable: true),
+                    monolingualism = table.Column<int>(type: "int", nullable: true),
+                    spelling = table.Column<int>(type: "int", nullable: true),
+                    overAllEnglish = table.Column<int>(type: "int", nullable: true),
+                    iduser = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scores", x => x.idscores);
+                    table.ForeignKey(
+                        name: "FK__Scores__idusuari__02084FDA",
+                        column: x => x.iduser,
+                        principalTable: "User",
+                        principalColumn: "iduser");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scores_iduser",
+                table: "Scores",
+                column: "iduser");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scores");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ EnglishUpgrade_Api/obj/Release/net6.0/PubTmp/EFSQLScripts/EnglishUpgradeApi.Models.EnglishTutoringContext.sql
@@ -0,0 +1,62 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220820015713_init')
+BEGIN
+    CREATE TABLE [User] (
+        [iduser] int NOT NULL IDENTITY,
+        [username] varchar(30) NULL,
+        [email] varchar(30) NULL,
+        [grouprank] varchar(30) NULL,
+        [companyrank] varchar(30) NULL,
+        [level] varchar(30) NULL,
+        CONSTRAINT [PK_Usuario] PRIMARY KEY ([iduser])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220820015713_init')
+BEGIN
+    CREATE TABLE [Scores] (
+        [idscores] int NOT NULL IDENTITY,
+        [yearParticipate] varchar(50) NULL,
+        [comprehension] int NULL,
+        [fluency] int NULL,
+        [sentenceStructure] int NULL,
+        [pronunciation] int NULL,
+        [monolingualism] int NULL,
+        [spelling] int NULL,
+        [overAllEnglish] int NULL,
+        [iduser] int NULL,
+        CONSTRAINT [PK_Scores] PRIMARY KEY ([idscores]),
+        CONSTRAINT [FK__Scores__idusuari__02084FDA] FOREIGN KEY ([iduser]) REFERENCES [User] ([iduser])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220820015713_init')
+BEGIN
+    CREATE INDEX [IX_Scores_iduser] ON [Scores] ([iduser]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220820015713_init')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220820015713_init', N'6.0.8');
+END;
+GO
+
+COMMIT;
+GO
+
